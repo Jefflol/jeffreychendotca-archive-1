6d63499d763898155ef56cabb73243df9578575c
Jeffrey Chen <jeffreychen246@gmail.com>
2020-12-16 22:10:46 -0800

    Add dark mode - Add dark mode toggle with animations - Support dark mode functionality

---
+++ src/components/DarkModeToggle/DarkModeToggle.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { ThemeContext } from '../ThemeContext';
+import { useSpring, animated } from "react-spring";
+import styles from "./DarkModeToggle.module.css";
+
+const properties = {
+    dark: {
+        r: 9,
+        transform: "rotate(40deg)",
+        cx: 12,
+        cy: 4,
+        opacity: 0
+    },
+    light: {
+        r: 5,
+        transform: "rotate(90deg)",
+        cx: 30,
+        cy: 0,
+        opacity: 1
+    },
+    springConfig: {
+        mass: 4,
+        tension: 250,
+        friction: 35
+    }
+};
+
+const DarkModeToggle = () => {
+    const { colorMode, setColorMode } = React.useContext(ThemeContext);
+    
+    const toggleDarkMode = () => {
+        setColorMode(colorMode === 'dark' ? 'light': 'dark');
+    };
+    
+    const { r, transform, cx, cy, opacity } = properties[
+        (colorMode === 'dark') ? 'light' : 'dark'
+    ];
+    
+    const svgContainerProps = useSpring({ transform, config: properties.springConfig });
+    const centerCircleProps = useSpring({ r, config: properties.springConfig });
+    const maskedCircleProps = useSpring({ cx, cy, config: properties.springConfig });
+    const linesProps = useSpring({ opacity, config: properties.springConfig });
+    
+    if (!colorMode) {
+        return null;
+    }
+
+    return (
+        <animated.svg
+            className={styles.icon}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            onClick={toggleDarkMode}
+            style={{ cursor: "pointer", ...svgContainerProps }}
+        >
+            <mask id="sunMask">
+                <rect x="0" y="0" width="100%" height="100%" fill="white" />
+                <animated.circle style={maskedCircleProps} r="9" fill="black" />
+            </mask>
+            <animated.circle
+                className={styles.sun}
+                style={centerCircleProps}
+                cx="12"
+                cy="12"
+                fill="currentColor"
+                mask="url(#sunMask)"
+            />
+            <animated.g 
+                className={styles.sunrays}
+                style={linesProps}
+                stroke="currentColor"
+            >
+                <line x1="12" y1="1" x2="12" y2="3" />
+                <line x1="12" y1="21" x2="12" y2="23" />
+                <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+                <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+                <line x1="1" y1="12" x2="3" y2="12" />
+                <line x1="21" y1="12" x2="23" y2="12" />
+                <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+                <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+            </animated.g>
+        </animated.svg>
+    );
+  };
+
+export default DarkModeToggle;
---
+++ src/constants.js
@@ -0,0 +1,29 @@
+export const COLORS = {
+  text: {
+    light: 'rgba(0, 0, 0, 1)',
+    dark: 'rgba(137, 137, 137, 1)',
+  },
+  background: {
+    light: 'rgba(245, 245, 245, 1)',
+    dark: 'rgba(29, 29, 29, 1)',
+  },
+  primary: {
+    light: 'rgba(0, 88, 133, 1)',
+    dark: 'rgba(81, 180, 205, 1)',
+  },
+  navbar: {
+    light: 'rgba(245, 245, 245, 1)',
+    dark: 'rgba(36, 36, 36, 1)'
+  },
+  brandName: {
+    light: 'rgba(29, 29, 29, 1)',
+    dark: 'rgba(81, 180, 205, 1)',
+  },
+  brandPosition: {
+    light: 'rgba(0, 88, 133, 1)',
+    dark: 'rgba(245, 245, 245, 1)',
+  },
+};
+
+export const COLOR_MODE_KEY = 'color-mode';
+export const INITIAL_COLOR_MODE_CSS_PROP = '--initial-color-mode';
