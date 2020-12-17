d6139c53c18d506e56c177ad8b89633478b8b806
Jeffrey Chen <jeffreychen246@gmail.com>
2020-12-16 22:16:11 -0800

    Clean up files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jeffrey Chen`,
+    description: `This is Jeffrey Chen.`,
+    author: `Jeffrey Chen`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
