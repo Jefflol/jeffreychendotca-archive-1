e434236fd4b8234ec9d6462e7472844f1d8c7b0f
Jeffrey Chen <jeffreychen246@gmail.com>
2020-12-17 21:02:44 -0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d17c7fec4a91050c5327.js"
+    "url": "webpack-runtime-ccc3e3fad98e295b6d09.js"
   },
   {
     "url": "framework-83e11af5cf111be5ad89.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e1752dbb2708bb345e2b05f1c97aa84b"
+    "revision": "8823f51e6ce42bb5cbbeaff4efe729c3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
