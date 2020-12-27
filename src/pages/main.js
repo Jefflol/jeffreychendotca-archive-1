9a1f9769dcd3c7616db6379f9af7b52389873734
Jeffrey Chen <jeffreychen246@gmail.com>
2020-12-26 17:21:29 -0800

    Add projects to main page - Add images for projects

--- src/pages/main.js
+++ src/pages/main.js
@@ -25,16 +25,40 @@ const MainPage = () => (
         </div>
         <div id="projects" className={styles.projects}>
             <div className={styles.project_layout} data-sal="fade" data-sal-delay="100" data-sal-duration="500" data-sal-easing="ease-in-out-cubic">
-                <Image className={styles.project__image} src="iphone_template_1024.png" alt="" />
+                <Image className={styles.project__image} src="m4f.png" alt="Mood4Food" />
                 <div className={styles.project__detail}>
-                    <ShiftBy x={-9}>
-                        <h2 className={styles.project__title}>TenKay</h2>
+                    <ShiftBy x={5}>
+                        <h2 className={styles.project__title}>M4F</h2>
                     </ShiftBy>
-                    <p className={styles.project__description}>Track your work. Review your progress.</p>
-                    <p className={styles.project__description}>A productivity mobile application built with <span className={styles.tech_highlight}>React Native</span>.</p>
+                    <p className={styles.project__description}>Check the safety hub before you get some grub.</p>
+                    <p className={styles.project__description}>A web application built with <span className={styles.tech_highlight}>Bootstrap</span> and <span className={styles.tech_highlight}>Firebase</span>.</p>
                     <div className={styles.projects__links}>
                         <Link className={styles.link} to="/">View Project</Link>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.project_layout} data-sal="fade" data-sal-delay="100" data-sal-duration="500" data-sal-easing="ease-in-out-cubic">
+                <Image className={styles.project__image} src="wordsy.png" alt="Wordsy" />
+                <div className={styles.project__detail}>
+                    <ShiftBy x={-9}>
+                        <h2 className={styles.project__title}>Wordsy</h2>
+                    </ShiftBy>
+                    <p className={styles.project__description}>Your personal word bank.</p>
+                    <p className={styles.project__description}>A web application built with <span className={styles.tech_highlight}>React</span> and <span className={styles.tech_highlight}>MongoDB</span>.</p>
+                    <div className={styles.projects__links}>
                         <Link className={styles.link} to="/">View Project</Link>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.project_layout} data-sal="fade" data-sal-delay="100" data-sal-duration="500" data-sal-easing="ease-in-out-cubic">
+                <Image className={styles.project__image} src="archive.png" alt="Archive" />
+                <div className={styles.project__detail}>
+                    <ShiftBy x={7}>
+                        <h2 className={styles.project__title}>Archive</h2>
+                    </ShiftBy>
+                    <p className={styles.project__description}>A collection of mini-projects.</p>
+                    <div className={styles.projects__links}>
+                        <Link className={styles.link} to="/">View Projects</Link>
                     </div>
                 </div>
             </div>
