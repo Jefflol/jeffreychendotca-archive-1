473a52116300bdd47f472c95df20794d4d0ae9e3
Jeffrey Chen <jeffreychen246@gmail.com>
2020-12-28 18:40:48 -0800

    Add images and details to archive projects

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 
 import Layout from "../components/Layout/Layout";
 import Image from "../components/Image";
@@ -11,6 +10,12 @@ import styles from "./styles/projectLayout.module.css";
 const ProjectArchivePage = () => {
     const goToLink = url => {
         window.location=`${url}`;
+    }
+
+    const handleKeyDown = (event, url) => {
+        if (event.keyCode === 13) {
+            goToLink(url);
+        }
     }
 
     return (
@@ -27,37 +32,41 @@ const ProjectArchivePage = () => {
                     </div>
                 </div>
                 <div className={styles.archive__detail}>
-                    <div className={styles.archive__section} onClick={() => goToLink("https://www.google.com")}>
+                    <div className={styles.archive__section} role={"button"} tabIndex={0} onClick={() => goToLink("https://github.com/Jefflol/palette-picker")} onKeyDown={(e) => handleKeyDown(e, "https://github.com/Jefflol/palette-picker")}>
                         <div className={styles.archive__section_detail}>
                             <h3 className={styles.archive__section_title}>Palette Picker</h3>
                             <p className={styles.archive__section_description}>Built with <span className={styles.tech_highlight}>HTML</span>, <span className={styles.tech_highlight}>CSS</span>, <span className={styles.tech_highlight}>NodeJS</span></p>
                             <p className={styles.archive__section_description}>A website showcasing different palettes.</p>
+                            <p className={styles.archive__section_description}><span className={styles.tech_highlight}>See Github</span></p>
                         </div>
-                        <Image className={styles.archive__image} src="archive.png" alt="Archive" />
+                        <Image className={styles.archive__image} src="palette_picker.png" alt="Palette Picker" />
                     </div>
-                    <div className={styles.archive__section}>
+                    <div className={styles.archive__section} role={"button"} tabIndex={0} onClick={() => goToLink("https://github.com/Jefflol/instant-recipe")} onKeyDown={(e) => handleKeyDown(e, "https://github.com/Jefflol/instant-recipe")}>
                         <div className={styles.archive__section_detail}>
                             <h3 className={styles.archive__section_title}>Instant Recipe</h3>
-                            <p className={styles.archive__section_description}>Built with <span className={styles.tech_highlight}>HTML</span>, <span className={styles.tech_highlight}>CSS</span>, <span className={styles.tech_highlight}>NodeJS</span></p>
-                            <p className={styles.archive__section_description}>A website showcasing different palettes.</p>
+                            <p className={styles.archive__section_description}>Built with <span className={styles.tech_highlight}>React</span> and <span className={styles.tech_highlight}>MongoDB</span></p>
+                            <p className={styles.archive__section_description}>A web application with a database of recipes.</p>
+                            <p className={styles.archive__section_description}><span className={styles.tech_highlight}>See Github</span></p>
                         </div>
-                        <Image className={styles.archive__image} src="archive.png" alt="Archive" />
+                        <Image className={styles.archive__image} src="instant_recipe.png" alt="Instant Recipe" />
                     </div>
-                    <div className={styles.archive__section}>
+                    <div className={styles.archive__section} role={"button"} tabIndex={0} onClick={() => goToLink("https://github.com/Jefflol/game-of-life")} onKeyDown={(e) => handleKeyDown(e, "https://github.com/Jefflol/game-of-life")}>
                         <div className={styles.archive__section_detail}>
                             <h3 className={styles.archive__section_title}>Game of Life</h3>
-                            <p className={styles.archive__section_description}>Built with <span className={styles.tech_highlight}>HTML</span>, <span className={styles.tech_highlight}>CSS</span>, <span className={styles.tech_highlight}>NodeJS</span></p>
-                            <p className={styles.archive__section_description}>A website showcasing different palettes.</p>
+                            <p className={styles.archive__section_description}>Built with <span className={styles.tech_highlight}>JavaScript</span>, <span className={styles.tech_highlight}>HTML</span>, <span className={styles.tech_highlight}>CSS</span></p>
+                            <p className={styles.archive__section_description}>A website demonstrating the game of life.</p>
+                            <p className={styles.archive__section_description}><span className={styles.tech_highlight}>See Github</span></p>
                         </div>
-                        <Image className={styles.archive__image} src="archive.png" alt="Archive" />
+                        <Image className={styles.archive__image} src="gol.png" alt="Game of Life" />
                     </div>
-                    <div className={styles.archive__section}>
+                    <div className={styles.archive__section} role={"button"} tabIndex={0} onClick={() => goToLink("https://github.com/Jefflol/maze-generator")} onKeyDown={(e) => handleKeyDown(e, "https://github.com/Jefflol/maze-generator")}>
                         <div className={styles.archive__section_detail}>
                             <h3 className={styles.archive__section_title}>Maze Generator</h3>
-                            <p className={styles.archive__section_description}>Built with <span className={styles.tech_highlight}>HTML</span>, <span className={styles.tech_highlight}>CSS</span>, <span className={styles.tech_highlight}>NodeJS</span></p>
-                            <p className={styles.archive__section_description}>A website showcasing different palettes.</p>
+                            <p className={styles.archive__section_description}>Built with <span className={styles.tech_highlight}>JavaScript</span>, <span className={styles.tech_highlight}>HTML</span>, <span className={styles.tech_highlight}>CSS</span></p>
+                            <p className={styles.archive__section_description}>A website demonstrating a maze generation.</p>
+                            <p className={styles.archive__section_description}><span className={styles.tech_highlight}>See Github</span></p>
                         </div>
-                        <Image className={styles.archive__image} src="archive.png" alt="Archive" />
+                        <Image className={styles.archive__image} src="maze_generator.png" alt="Maze Generator" />
                     </div>
                 </div>
             </div>
