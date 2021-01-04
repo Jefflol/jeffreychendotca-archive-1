e62d7ad34778b9abfbe42715950828fc3a0fbd17
Jeffrey Chen <jeffreychen246@gmail.com>
2021-01-03 21:51:34 -0800

    Remove unused code

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,36 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
-// import { useStaticQuery, graphql } from "gatsby"
 
 import Navbar from "./Navbar"
-// import Header from "./Header"
 import "./Layout.css"
 import styles from "./Layout.module.css";
 
 const Layout = ({ children }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-
   return (
     <div className={styles.container}>
       <Navbar />
-      {/* <Header siteTitle={data.site.siteMetadata?.title || `Title`} /> */}
       <div className={styles.layout}>
         <main>{children}</main>
-        {/* <footer style={{
-          marginTop: `2rem`
-        }}>
-          <small style={{
-            fontSize: `0.8rem`
-          }}>© 2020-present Jeffrey Chen. All Rights Reserved.</small>
-        </footer> */}
       </div>
     </div>
   )
