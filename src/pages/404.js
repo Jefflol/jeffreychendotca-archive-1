fb3811daa636ac57523e3e95b4881432d37927ea
Jeffrey Chen <jeffreychen246@gmail.com>
2021-01-03 21:49:47 -0800

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,16 +4,16 @@ import SEO from "../components/seo"
 import DarkModeToggle from "../components/DarkModeToggle/DarkModeToggle"
 
 import styles from "./styles/index.module.css";
+import { Anchor } from "../components/components";
 
 const NotFoundPage = () => (
   <>
-    <SEO title="Hi" />
+    <SEO title="404" />
     <div className={styles.page}>
       <DarkModeToggle />
-      <h1 className={styles.brand__name}>Jeffrey Chen</h1>
-      <h3 className={styles.brand__position}>Front End Developer</h3>
-      <h5 className={[styles.brand__description, styles.text_primary].join(' ')}>who dabbles in back end from time to time.</h5>
-      <p className={[styles.brand__contact, styles.text_primary].join(' ')}>Connect with me on <a href="https://www.linkedin.com/in/jeffreychen246">LinkedIn</a>.</p>
+      <h1>404</h1>
+      <Anchor to="/">Return to Home</Anchor>
+      <p className={styles.brand__connect}>Connect with me on <a href="https://www.linkedin.com/in/jeffreychen246">LinkedIn</a>.</p>
     </div>
   </>
 )
