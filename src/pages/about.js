44961513de576fef1020663d3d3543c0444a4d6b
Jeffrey Chen <jeffreychen246@gmail.com>
2021-01-04 16:39:57 -0800

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,7 +25,10 @@ const AboutPage = () => (
             <ShiftBy x={5}>
                 <h1 className={styles.about__title}>About Me</h1>
             </ShiftBy>
+            <p className={styles.about__description}>Hi there! I'm Jeffrey. I'm a passionate intervert who loves developing designs with code.</p>
             <p className={styles.about__description}>I enjoy both front end and back end development but lately I've been working with front end more. Right now, I'm learning React and building my portfolio with it!</p>
+            <p className={styles.about__description}>I'm a student seeking for an 8-month co-op position starting May 2021.</p>
+
             <div className={styles.about__social}>
                 <Anchor className={styles.link} to="/">Resume</Anchor>
                 <Anchor className={styles.link} to="https://www.linkedin.com/in/jeffreychen246">LinkedIn</Anchor>
