deaf20a946c3547f0be36a4c641d83b9780c8821
Jeffrey Chen <jeffreychen246@gmail.com>
2021-01-13 22:47:38 -0800

    Add new about description

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,10 +25,9 @@ const AboutPage = () => (
             <ShiftBy x={5}>
                 <h1 className={styles.about__title}>About Me</h1>
             </ShiftBy>
-            <p className={styles.about__description}>Hi there! I'm Jeffrey. I'm a passionate intervert who loves developing designs with code.</p>
-            <p className={styles.about__description}>I enjoy both front end and back end development but lately I've been working with front end more. Right now, I'm learning React and building my portfolio with it!</p>
-            <p className={styles.about__description}>I'm a student seeking for an 8-month co-op position starting May 2021.</p>
-
+            <p className={styles.about__description}>Hi there! I'm Jeffrey. I have a previous degree in Engineering but a strong passion for front-end development.</p>
+            <p className={styles.about__description}>I consider myself a life-long student who is always ready to learn and is motivated by designing and developing designs into products.</p>
+            <p className={styles.about__description}>I'm available for an 8-month co-op position starting May 2021.</p>
             <div className={styles.about__social}>
                 <Anchor className={styles.link} href="https://drive.google.com/file/d/1nEBWVpVY5HB3Y0MtqtdD7J-poSmDr-VW/view?usp=sharing">Resume</Anchor>
                 <Anchor className={styles.link} href="https://www.linkedin.com/in/jeffreychen246">LinkedIn</Anchor>
